73b6530410c0515148ccff57d7df4cca29450197
Jacob <EarthTek19@gmail.com>
2024-12-13 14:00:03 -0600

    fixed association issue on review

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Review.belongsTo(models.Spot, { foreignKey: 'spotId' });
       Review.belongsTo(models.User, { foreignKey: 'userId' });
-      Review.hasMany(Image, {
+      Review.hasMany(models.Image, {
         foreignKey: 'imageableId',
         constraints: false,
         scope: {
