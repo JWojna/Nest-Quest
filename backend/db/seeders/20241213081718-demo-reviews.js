d5f20960062a540d42028a0b41141a544ccf3674
Jacob <EarthTek19@gmail.com>
2024-12-15 21:35:01 -0600

    initial seed test complete, added data dir to house larger seed data sets + populated user/spot datafiles

--- backend/db/seeders/20241213081718-demo-reviews.js
+++ backend/db/seeders/20241213081718-demo-reviews.js
@@ -1,25 +1,31 @@
 'use strict';
 
+const { Review } = require('../models');
+
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await Review.bulkCreate([
+      {
+        userId: 1,
+        spotId: 2,
+        review: 'This was an awesome place!',
+        stars: 5,
+      },
+      {
+        userId: 2,
+        spotId: 2,
+        review: 'This was an awesome!',
+        stars: 4,
+      },
+    ], { validate: true });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('Reviews')
   }
 };
--- backend/db/seeders/20241213081727-demo-images.js
+++ backend/db/seeders/20241213081727-demo-images.js
@@ -1,25 +1,27 @@
 'use strict';
 
+const { Image } = require('../models');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await Image.bulkCreate([
+      {
+        url: 'fakeimage.com',
+        preview: true,
+        imageableId: 2,
+        imageableType: 'spot'
+      },
+      {
+        url: 'fakeimage.com',
+        preview: true,
+        imageableId: 2,
+        imageableType: 'review'
+      },
+    ], { validate: true });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return queryInterface.bulkDelete('Images')
   }
 };
