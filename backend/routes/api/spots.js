1b7a1d0b9b7cf56077117ca75fca066ab6321b1a
Jacob <EarthTek19@gmail.com>
2024-12-20 21:58:07 -0600

    finished get spot details by spotId endpoint

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,6 +1,6 @@
 //^ backend/routes/api/spots.js
 const express = require('express');
-const { Spot, Image, Review } = require('../../db/models');
+const { Spot, Image, Review, User } = require('../../db/models');
 const { Op, fn, col } = require('sequelize');
 
 
@@ -71,5 +71,69 @@ router.get('/', async (req, res) => {
     };
 
 });
+
+
+//~ GET SPOT DETAILS BY SPOT ID
+router.get('/:spotId', async (req, res) => {
+    const spotId = req.params.spotId;
+    try {
+        const spot = await Spot.findByPk(spotId);
+
+        const owner = await User.findByPk(spot.ownerId)
+
+        const spotImages = await Image.findAll({
+            where: {
+                imageableType: 'spot',
+                imageableId: spotId
+            }
+        })
+
+        const reviews = await Review.findAll({
+            where: { spotId: spotId},
+            attributes: ['spotId', [fn('SUM', col('stars')), 'sumStars'], [fn('COUNT', col('stars')), 'reviewCount']],
+            group: ['spotId']
+        });
+
+        let sumStars;
+        let reviewCount;
+
+        const  { sumStars: totalStars, reviewCount: totalCount } = reviews[0].dataValues;
+        sumStars = totalStars;
+        reviewCount = totalCount;
+
+
+        const { createdAt, updatedAt, ...spotData } = spot.get();
+
+        const responseData = {
+            ...spotData,
+            createdAt: formatDate(spot.createdAt),
+            updatedAt: formatDate(spot.updatedAt),
+            numRatings: reviewCount,
+            avgStarRating: sumStars / reviewCount || null,
+            spotImages: spotImages.map( image => ({
+                id: image.id,
+                url: image.url,
+                preview: image.preview
+            })) || null,
+            Owner: {
+                id: owner.id,
+                firstName: owner.firstName,
+                lastName: owner.lastName
+            }
+        }
+
+
+
+        return res.json(responseData);
+    } catch (error) {
+        console.error('Error fetching spot:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+})
+
+
+
+
 
 module.exports = router;
