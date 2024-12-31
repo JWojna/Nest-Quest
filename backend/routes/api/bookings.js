19acebaba12cc7bcb52edd29771536d2bf88d2a5
Jacob <EarthTek19@gmail.com>
2024-12-31 17:43:26 -0600

    edit bookings route, authed/owned check, succesfully updates record

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -2,12 +2,51 @@ const express = require('express');
 const { Spot, Image, Review, User, Booking } = require('../../db/models');
 const { requireAuth, checkOwnership } = require('../../utils/auth');
 const formatDate = require('../api/utils/date-formatter');
-const { json } = require('sequelize');
+
 
 const router = express.Router();
 
+//^ get curr date for checks
+const currDate = new Date();
+
 //~EDIT A BOOKING
 //! require auth and ownership
+router.put('/:bookingId', requireAuth, checkOwnership(Booking, 'bookingId', 'userId'),
+async (req, res) => {
+    const booking = await Booking.findByPk(req.params.bookingId);
+    if (!booking) return res.status(404).json({ 'message': `Booking couildn't be found` });
+
+    try {
+        booking.set({
+            ...req.body
+        });
+
+        booking.save({ validate: true });
+
+        const responseData = booking.get()
+        responseData.startDate = formatDate(responseData.startDate).split(' ')[0];
+        responseData.endDate = formatDate(responseData.endDate).split(' ')[0];
+        responseData.createdAt = formatDate(responseData.createdAt);
+        responseData.updatedAt = formatDate(responseData.updatedAt);
+
+        const resRemodel = {
+            id: responseData.id,
+            spotId: responseData.spotId,
+            userId: responseData.userId,
+            startDate: responseData.startDate,
+            endDate: responseData.endDate,
+            createdAt: responseData.createdAt,
+            updatedAt: responseData.updatedAt
+          }
+
+        res.json(resRemodel)
+    } catch (error) {
+        console.error('Error editing booking:', error);
+        return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+
+})
 
 
 //~DELETE A BOOKING
@@ -17,8 +56,6 @@ async (req, res) => {
     const booking = await Booking.findByPk(req.params.bookingId);
 
     if (!booking) return res.status(404).json({ 'message': `Booking couildn't be found` });
-
-    const currDate = new Date();
 
     if (booking.startDate <= currDate && booking.endDate >= currDate) {
         return res.status(403).json({ 'message': `Bookings that have been started can't be deleted`});
