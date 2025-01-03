19472a0fa63eae71457c3fb3f9c88abe1ad77855
Jacob <EarthTek19@gmail.com>
2025-01-02 21:44:43 -0600

    fixed another spelling error

--- backend/db/migrations/20241213014437-create-spot.js
+++ backend/db/migrations/20241213014437-create-spot.js
@@ -73,7 +73,7 @@ module.exports = {
     }, options);
 
     //? add index here
-    await queryInterface.addIndex('NEST_QUEST_SCHEMA.Spots', ['userId', 'spotId'], {
+    await queryInterface.addIndex('NEST_QUEST_SCHEMA.Spots', ['price', 'lat', 'lng'], {
       name: 'idxUserIdSpotId',
       unique: true
     }, options);
@@ -81,7 +81,7 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     //? rmv index pre table drop
-    await queryInterface.removeIndex('NEST_QUEST_SCHEMA.Spots', 'idxUserIdSpotId');
+    await queryInterface.removeIndex('NEST_QUEST_SCHEMA.Spots', 'idxPriceLatLng');
     await queryInterface.dropTable('Spots', options);
   }
 };
--- backend/db/migrations/20241213014452-create-review.js
+++ backend/db/migrations/20241213014452-create-review.js
@@ -55,7 +55,7 @@ module.exports = {
     }, options);
 
     //? add indx
-    await queryInterface.addIndex('NEST_QUEST_SCHEMA.Reviews', ['price', 'lat', 'lng'], {
+    await queryInterface.addIndex('NEST_QUEST_SCHEMA.Reviews', ['userId', 'spotId'], {
       name: 'idxPriceLatLng',
       unique: false
     }, options);
@@ -63,7 +63,7 @@ module.exports = {
   },
   async down(queryInterface, Sequelize) {
     //? rmv idx
-    await queryInterface.removeIndex('NEST_QUEST_SCHEMA.Review', 'idxPriceLatLng');
+    await queryInterface.removeIndex('NEST_QUEST_SCHEMA.Review', 'idxUserIdSpotId');
     await queryInterface.dropTable('Reviews', options);
   }
 };
