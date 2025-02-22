74f72501891093f3194597c5f3d941de07defdc7
Jacob <EarthTek19@gmail.com>
2025-02-21 21:35:08 -0600

    completed login form component

---
+++ frontend/src/store/session.js
@@ -0,0 +1,45 @@
+import { csrfFetch } from "./csrf";
+
+const SET_SESSION_USER = 'session/SET_SESSION_USER';
+const REMOVE_SESSION_USER = 'session/REMOVE_SESSION_USER';
+
+//action creators
+const setSessionUser = (user) => ({
+    type: SET_SESSION_USER,
+    payload: user,
+});
+
+const removeSessionUser = () => ({
+    type: REMOVE_SESSION_USER,
+});
+
+// thunk action
+export const login = (user) => async (dispatch) => {
+    const { credential, password } = user;
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password
+        }),
+    });
+    const data = await response.json();
+    dispatch(setSessionUser(data.user));
+    return response;
+};
+
+//Reducers
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_SESSION_USER:
+            return {...state, user: action.user };
+        case REMOVE_SESSION_USER:
+            return {...state, user: null };
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
