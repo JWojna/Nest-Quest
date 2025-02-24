48824f9bf749584e62eb10a7bf9198a8acfef4e5
Jacob <EarthTek19@gmail.com>
2025-02-23 18:30:52 -0600

    added action and reducer logic for spot details page

--- frontend/src/components/SpotCard/SpotCard.jsx
+++ frontend/src/components/SpotCard/SpotCard.jsx
@@ -1,20 +1,24 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons'
-import './SpotCard.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import "./SpotCard.css";
 
 function SpotCard({ spot }) {
-    return (
-        <div className='spot-card'>
-            <img src={spot.previewImage} alt={spot.name} className='spot-card-image' />
-            <div className='spot-card-contnet'>
-                <h3>{spot.name}</h3>
-                <div className='spot-card-rating'>
-                    <FontAwesomeIcon icon={faStar} /> {spot.avgRating}
-                </div>
-                <div className='spot-card-price'>${spot.price}</div>
-            </div>
+  return (
+    <div className="spot-card">
+      <img
+        src={spot.previewImage}
+        alt={spot.name}
+        className="spot-card-image"
+      />
+      <div className="spot-card-contnet">
+        <div className="spot-card-rating">
+          <h3>{spot.name}</h3>
+          <FontAwesomeIcon icon={faStar} /> {spot.avgRating}
         </div>
-    )
+        <div className="spot-card-price">${spot.price} / night</div>
+      </div>
+    </div>
+  );
 }
 
 export default SpotCard;
--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -1,11 +1,17 @@
 const SET_SPOTS = 'spots/SET_SPOTS';
 const ADD_SPOTS = 'spots/ADD_SPOT';
 const REMOVE_SPOT = 'spots/REMOVE_SPOT';
+const SET_SPOT_DETAILS = 'spots/SET_SPOT_DETAILS';
 
 // Action creators
 const setSpots = (spots) => ({
     type: SET_SPOTS,
     payload: spots
+});
+
+const setSpot = (spot) => ({
+    type: SET_SPOT_DETAILS,
+    payload: spot
 });
 
 // const addSpot = (spot) => ({
@@ -18,26 +24,37 @@ const setSpots = (spots) => ({
 //     payload: spotId
 // });
 
-// thunk actions to fetch spots
+// thunk actions
 export const fetchSpots = () => async (dispatch) => {
     const response = await fetch('/api/spots');
     if (!response.ok) throw new Error('failed to fetch spots');
 
     const data = await response.json()
-    // console.log(data);
     dispatch(setSpots(data.Spots));
     return response;
 };
 
+export const fetchSpotDetails = (spotId) => async (dispatch) => {
+    const response = await fetch(`/api/spots/${spotId}`);
+    if (!response.ok) throw new Error('failed to fetch spot');
+
+    const data = await response.json()
+    dispatch(setSpot(data.Spot));
+    return response;
+}
+
 // reducer
 const initialState = {
-    spots: []
+    spots: [],
+    spot: []
 };
 
 const spotsReducer = (state = initialState, action) => {
     switch (action.type) {
         case SET_SPOTS:
             return { ...state, spots: action.payload };
+        case SET_SPOT_DETAILS:
+            return{ ...state, spot: { ...state.spot, [action.payload.id]: action.payload }}
         case ADD_SPOTS:
             return { ...state, spots: [...state.spots, action.payload] };
         case REMOVE_SPOT:
